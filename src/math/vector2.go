14ef36161d629ae8b33df26dd77e15146c86eaa9
Emma Bobola <embobola@gmail.com>
2017-04-07 14:50:26 -0400

    Vector2 class and tests written

--- src/math/vector2.go
+++ src/math/vector2.go
@@ -10,6 +10,62 @@ type Vector2 struct {
 	X, Y float64
 }
 
+// Componentwise equals
 func (m Vector2) Equals(n Vector2) bool {
 	return (math.Abs(m.X - n.X) + math.Abs(m.Y - n.Y)) == 0
+}
+
+// Componentwise addition
+func (m Vector2) Add(n Vector2) Vector2 {
+	return Vector2{m.X + n.X, m.Y + n.Y}
+}
+
+// Componentwise subtraction
+func (m Vector2) Subtract(n Vector2) Vector2 {
+	return Vector2{m.X - n.X, m.Y - n.Y}
+}
+
+// Componentwise multiplication
+func (m Vector2) Multiply(n Vector2) Vector2 {
+	return Vector2{m.X * n.X, m.Y * n.Y}
+}
+
+// Componentwise division
+func (m Vector2) Divide(n Vector2) Vector2 {
+	return Vector2{m.X / n.X, m.Y / n.Y}
+}
+
+// Componentwise scalar addition
+func (m Vector2) AddScalar(k float64) Vector2 {
+	return Vector2{m.X + k, m.Y + k}
+}
+
+// Componentwise scalar subtraction
+func (m Vector2) SubtractScalar(k float64) Vector2 {
+	return Vector2{m.X - k, m.Y - k}
+}
+
+// Componentwise scalar multiplication
+func (m Vector2) MultiplyScalar(k float64) Vector2 {
+	return Vector2{m.X * k, m.Y * k}
+}
+
+// Componentwise scalar division
+func (m Vector2) DivideScalar(k float64) Vector2 {
+	return Vector2{m.X / k, m.Y / k}
+}
+
+// Gets Magnitude
+func (m Vector2) Magnitude() float64 {
+	return Sqrt(Pow(m.X, 2) + Pow(m.Y, 2))
+}
+
+// Gets Magnitude of m < n
+func (m Vector2) LessThan(n Vector2) bool {
+	return m.Magnitude() < n.Magnitude()
+}
+
+// Gets Magnitude of m > n
+func (m Vector2) GreaterThan(n Vector2) bool {
+	return m.Magnitude() > n.Magnitude()
 }
--- src/math/vector2_t.go
+++ src/math/vector2_t.go
@@ -11,3 +11,92 @@ func TestVector2Equal(test *testing.T) {
 		test.Fail()
 	}
 }
+
+func TestVector2Add(test *testing.T) {
+	v := Vector2{1,2}
+	if !(v.Add(Vector2{2,3}).Equals(Vector2{3,5})) {
+		test.Fail()
+	}
+}
+
+func TestVector2Subtract(test *testing.T) {
+	v := Vector2{1,2}
+	if !(v.Subtract(Vector2{2,2}).Equals(Vector2{-1,0})) {
+		test.Fail()
+	}
+}
+
+func TestVector2Multiply(test *testing.T) {
+	v := Vector2{1,2}
+	if !(v.Multiply(Vector2{4,2}).Equals(Vector2{4,4})) {
+		test.Fail()
+	}
+}
+
+func TestVector2Divide(test *testing.T) {
+	v := Vector2{3,8}
+	if !(v.Divide(Vector2{2,4}).Equals(Vector2{1.5,2})) {
+		test.Fail()
+	}
+}
+
+func TestVector2AddScalar(test *testing.T) {
+	v := Vector2{1,3}
+	if !(v.AddScalar(2).Equals(Vector2{3,5})) {
+		test.Fail()
+	}
+}
+
+func TestVector2SubtractScalar(test *testing.T) {
+	v := Vector2{1,2}
+	if !(v.SubtractScalar(2).Equals(Vector2{-1,0})) {
+		test.Fail()
+	}
+}
+
+func TestVector2MultiplyScalar(test *testing.T) {
+	v := Vector2{1,2}
+	if !(v.MultiplyScalar(3).Equals(Vector2{3,6})) {
+		test.Fail()
+	}
+}
+
+func TestVector2DivideScalar(test *testing.T) {
+	v := Vector2{3,8}
+	if !(v.DivideScalar(2).Equals(Vector2{1.5,4})) {
+		test.Fail()
+	}
+}
+
+func TestVector2Magnitude(test *testing.T) {
+	v := Vector{3,4}
+	if !(v.Magnitude() == 5) {
+		test.Fail()
+	}
+}
+
+func TestVector2LessThan(test *testing.T) {
+	v := Vector{1,2}
+	if v.LessThan(Vector2{0,2}) {
+		test.Fail()
+	}
+	if v.LessThan(v) {
+		test.Fail()
+	}
+	if !v.LessThan(Vector2{2,3}) {
+		test.Fail()
+	}
+}
+
+func TestVector2GreaterThan(test *testing.T) {
+	v := Vector2{1,2}
+	if v.GreaterThan(Vector2{1,3}) {
+		test.Fail()
+	}
+	if v.GreaterThan(v) {
+		test.Fail()
+	}
+	if !v.GreaterThan(Vector2{0,2}) {
+		test.Fail()
+	}
+}
