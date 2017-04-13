88e4deebf4b12702bf63aaa1840075ab6bcbd185
Emma Bobola <embobola@gmail.com>
2017-04-13 14:37:59 -0400

    self modification added to vectors

--- src/math/vector2.go
+++ src/math/vector2.go
@@ -25,9 +25,21 @@ func (m Vector2) Add(n Vector2) Vector2 {
 	return Vector2{m.X + n.X, m.Y + n.Y}
 }
 
+// Self componentwise addition
+func (m *Vector2) Add(n Vector2) {
+	m.X += n.X
+	m.Y += n.Y
+}
+
 // Componentwise subtraction
 func (m Vector2) Subtract(n Vector2) Vector2 {
 	return Vector2{m.X - n.X, m.Y - n.Y}
+}
+
+// Self componentwise subtraction
+func (m *Vector2) Subtract(n Vector2) {
+	m.X -= n.X
+	m.Y -= n.Y
 }
 
 // Componentwise multiplication
@@ -35,9 +47,21 @@ func (m Vector2) Multiply(n Vector2) Vector2 {
 	return Vector2{m.X * n.X, m.Y * n.Y}
 }
 
+// Self componentwise multiplication
+func (m *Vector2) Multiply(n Vector2) {
+	m.X *= n.X
+	m.Y *= n.Y
+}
+
 // Componentwise division
 func (m Vector2) Divide(n Vector2) Vector2 {
 	return Vector2{m.X / n.X, m.Y / n.Y}
+}
+
+// Self componentwise division
+func (m *Vector2) Divide(n Vector2)  {
+	m.X /= n.X
+	m.Y /= n.Y
 }
 
 // Componentwise scalar addition
@@ -45,9 +69,21 @@ func (m Vector2) AddScalar(k float64) Vector2 {
 	return Vector2{m.X + k, m.Y + k}
 }
 
+// Self componentwise scalar addition
+func (m *Vector2) AddScalar(k float64) {
+	m.X += k
+	m.Y += k
+}
+
 // Componentwise scalar subtraction
 func (m Vector2) SubtractScalar(k float64) Vector2 {
 	return Vector2{m.X - k, m.Y - k}
+}
+
+// Self componentwise scalar subtraction
+func (m *Vector2) SubtractScalar(k float64) {
+	m.X -= k
+	m.Y -= k
 }
 
 // Componentwise scalar multiplication
@@ -55,9 +91,21 @@ func (m Vector2) MultiplyScalar(k float64) Vector2 {
 	return Vector2{m.X * k, m.Y * k}
 }
 
+// Self componentwise scalar multiplication
+func (m *Vector2) MultiplyScalar(k float64) {
+	m.X *= k
+	m.Y *= k
+}
+
 // Componentwise scalar division
 func (m Vector2) DivideScalar(k float64) Vector2 {
 	return Vector2{m.X / k, m.Y / k}
+}
+
+// Self componentwise scalar division
+func (m *Vector2) DivideScalar(k float64) {
+	m.X /= k
+	m.Y /= k
 }
 
 // Gets Magnitude
