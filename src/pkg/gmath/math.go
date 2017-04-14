4cd71145226ea559e71e48404dbbd2721dc56817
Emma Bobola <embobola@gmail.com>
2017-04-14 15:25:43 -0400

    added package folder

---
+++ src/pkg/gmath/math.go
@@ -0,0 +1,15 @@
+package gmath
+
+import "math"
+
+func Equal(x, y float64) bool {
+	return x == y
+}
+
+// Lerp (0, 2, 0) = 0
+// Lerp (0, 2, 0.5) = 1
+// Lerp (0, 2, 1) = 2
+
+func Lerp(x, y, t float64) float64 {
+	return (1-t)*x + t*y
+}
